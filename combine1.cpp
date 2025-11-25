264872456508a4933d4394ef59eab8dac8dc0c43
Onur Vallor <onurvallor@gmail.com>
2025-11-25 18:01:09 -0500

    Added for-loop and function to retrieve element from vector

--- combine1.cpp
+++ combine1.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <stdexcept>
 #include <vector>
+int get_elem(std::vector<int> &v, int index) {
+
+  if (index >= v.size()) {
+    throw std::out_of_range("Index is out of range of vector.");
+  }
+
+  return v[index];
+}
+int combine1(std::vector<int> &v) {
+
+  int acc = 1;
+
+  for (size_t i = 0; i < v.size(); i++) {
+    int x = get_elem(v, i);
+
+    acc *= x;
+  }
+
+  return acc;
+}
 
 int main() {
   std::vector<int> numVector;
+
   numVector.push_back(1);
   numVector.push_back(2);
   numVector.push_back(3);
+
+  int acc_result = combine1(numVector);
+
+  std::cout << "Accumulated value from the vector " << acc_result << std::endl;
   return 0;
 }
