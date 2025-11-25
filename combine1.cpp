b9a9730d4b5dc9e030175b063ce04a19f01e3130
Onur Vallor <onurvallor@gmail.com>
2025-11-25 17:12:48 -0500

    Initial combine1

---
+++ combine1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<int> numVector;
+  numVector.push_back(1);
+  numVector.push_back(2);
+  numVector.push_back(3);
+  return 0;
+}
